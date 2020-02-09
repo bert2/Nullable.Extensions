c51e9389199373f3f7b6f7c68b8b44a06b9b43b3
bert2 <shuairan@gmail.com>
2020-02-09 15:53:57 +0100

    restructure tests a bit (without release, +semver:skip)

--- tests/UnitTests/SwitchTests.cs
+++ tests/UnitTests/SwitchTests.cs
@@ -2,22 +2,35 @@ namespace UnitTests {
     using Xunit;
     using Nullable.Extensions;
     using Shouldly;
+    using System;
 
     public static class SwitchTests {
         public class Class {
+            [Fact] public void EvaluatesValueHandlerWhenGivenValue()
+                => "hi".Switch(notNull: _ => true, isNull: () => false).ShouldBeTrue();
+
             [Fact] public void EvaluatesNullHandlerWhenGivenNull()
                 => ((string?)null).Switch(notNull: _ => true, isNull: () => false).ShouldBeFalse();
 
-            [Fact] public void EvaluatesValueHandlerWhenGivenValue()
-                => "hi".Switch(notNull: _ => true, isNull: () => false).ShouldBeTrue();
+            [Fact] public void CanThrowExceptionWhenGivenNull() => new Action(() =>
+                ((string?)null).Switch(
+                    notNull: _ => true,
+                    isNull: () => throw new InvalidOperationException()))
+                .ShouldThrow<InvalidOperationException>();
         }
 
         public class Struct {
+            [Fact] public void EvaluatesValueHandlerWhenGivenValue()
+                => ((int?)5).Switch(notNull: _ => true, isNull: () => false).ShouldBeTrue();
+
             [Fact] public void EvaluatesNullHandlerWhenGivenNull()
                 => ((int?)null).Switch(notNull: _ => true, isNull: () => false).ShouldBeFalse();
 
-            [Fact] public void EvaluatesValueHandlerWhenGivenValue()
-                => ((int?)5).Switch(notNull: _ => true, isNull: () => false).ShouldBeTrue();
+            [Fact] public void CanThrowExceptionWhenGivenNull() => new Action(() =>
+                ((int?)null).Switch(
+                    notNull: _ => true,
+                    isNull: () => throw new InvalidOperationException()))
+                .ShouldThrow<InvalidOperationException>();
         }
     }
 }
