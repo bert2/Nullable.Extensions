b18bf0e73c615c42d0cafe6a09c3b8e6e752dc66
bert2 <shuairan@gmail.com>
2020-03-01 14:34:18 +0100

    really unit-test `SelectMany()` alias for `Bind()` (+semver:skip, without release)

--- tests/UnitTests/SelectMany1Tests.cs
+++ tests/UnitTests/SelectMany1Tests.cs
@@ -6,52 +6,52 @@ namespace UnitTests {
     public static class SelectMany1Tests {
         public class Class {
             [Fact] public void ReturnsNullWhenGivenNull()
-                => ((string?)null).Bind(GetFirstThreeLetters).ShouldBeNull();
+                => ((string?)null).SelectMany(GetFirstThreeLetters).ShouldBeNull();
 
             [Fact] public void ReturnsNullWhenBinderReturnsNull()
-                => "12".Bind(GetFirstThreeLetters).ShouldBeNull();
+                => "12".SelectMany(GetFirstThreeLetters).ShouldBeNull();
 
             [Fact] public void ReturnsBinderResult()
-                => "1234".Bind(GetFirstThreeLetters).ShouldBe("123");
+                => "1234".SelectMany(GetFirstThreeLetters).ShouldBe("123");
 
             private static string? GetFirstThreeLetters(string s) => s.Length >= 3 ? s[..3] : null;
         }
 
         public class Struct {
             [Fact] public void ReturnsNullWhenGivenNull()
-                => ((double?)null).Bind(x => Divide(x, 3)).ShouldBeNull();
+                => ((double?)null).SelectMany(x => Divide(x, 3)).ShouldBeNull();
 
             [Fact] public void ReturnsNullWhenBinderReturnsNull()
-                => ((double?)6).Bind(x => Divide(x, 0)).ShouldBeNull();
+                => ((double?)6).SelectMany(x => Divide(x, 0)).ShouldBeNull();
 
             [Fact] public void ReturnsBinderResult()
-                => ((double?)6).Bind(x => Divide(x, 3)).ShouldBe(2);
+                => ((double?)6).SelectMany(x => Divide(x, 3)).ShouldBe(2);
 
             private static double? Divide(double dividend, double divisor) => divisor == 0 ? (double?)null : dividend / divisor;
         }
 
         public class Class2Struct {
             [Fact] public void ReturnsNullWhenGivenNull()
-                => ((string?)null).Bind(Parse).ShouldBeNull();
+                => ((string?)null).SelectMany(Parse).ShouldBeNull();
 
             [Fact] public void ReturnsNullWhenBinderReturnsNull()
-                => "abc".Bind(Parse).ShouldBeNull();
+                => "abc".SelectMany(Parse).ShouldBeNull();
 
             [Fact] public void ReturnsBinderResult()
-                => "123".Bind(Parse).ShouldBe(123);
+                => "123".SelectMany(Parse).ShouldBe(123);
 
             private static int? Parse(string s) => int.TryParse(s, out var i) ? i : (int?)null;
         }
 
         public class Struct2Class {
             [Fact] public void ReturnsNullWhenGivenNull()
-                => ((int?)null).Bind(PrintPositive).ShouldBeNull();
+                => ((int?)null).SelectMany(PrintPositive).ShouldBeNull();
 
             [Fact] public void ReturnsNullWhenBinderReturnsNull()
-                => ((int?)-13).Bind(PrintPositive).ShouldBeNull();
+                => ((int?)-13).SelectMany(PrintPositive).ShouldBeNull();
 
             [Fact] public void ReturnsBinderResult()
-                => ((int?)123).Bind(PrintPositive).ShouldBe("123");
+                => ((int?)123).SelectMany(PrintPositive).ShouldBe("123");
 
             private static string? PrintPositive(int i) => i > 0 ? i.ToString() : null;
         }
