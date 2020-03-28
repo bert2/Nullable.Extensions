4cb24b6f8b749f0ad0455555865767d13acdf74e
bert2 <shuairan@gmail.com>
2020-03-28 11:56:57 +0100

    use `Func<T,bool>` instead of `Predicate<T>` in `Filter()` +semver:patch

--- src/Nullable.Extensions/FilterExt.cs
+++ src/Nullable.Extensions/FilterExt.cs
@@ -2,11 +2,11 @@
     using System;
 
     public static class FilterExt {
-        public static T? Filter<T>(this T? x, Predicate<T> predicate)
+        public static T? Filter<T>(this T? x, Func<T, bool> predicate)
             where T : class
             => x != null && predicate(x) ? x : null;
 
-        public static T? Filter<T>(this T? x, Predicate<T> predicate)
+        public static T? Filter<T>(this T? x, Func<T, bool> predicate)
             where T : struct
             => x.HasValue && predicate(x.Value) ? x : null;
     }
